23f19a2c7d8e4d65428d2a82ac72c7b5d4b2cce7
karinaperes <karinaperesg@gmail.com>
2024-04-29 18:13:20 -0300

    criacao-usuarios

--- src/routes/routes.js
+++ src/routes/routes.js
@@ -3,6 +3,7 @@ const alunoRoutes = require("./alunos.routes")
 const cursoRoutes = require("./cursos.routes")
 const professorRoutes = require("./profesores.routes")
 const loginRoutes = require("./login.routes")
+const usuarioRoutes = require("./usuarios.routes")
 
 const routes = Router()
 
@@ -10,5 +11,6 @@ routes.use('/alunos', alunoRoutes)
 routes.use('/cursos', cursoRoutes)
 routes.use('/professores', professorRoutes)
 routes.use('/login', loginRoutes)
+routes.use('/usuarios', usuarioRoutes)
 
 module.exports = routes
